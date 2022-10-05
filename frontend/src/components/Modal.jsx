50112b77d25e9fb8081449188cb2668efc9e3e17
Eslam10497 <eslam.wael104@gmail.com>
2022-10-05 04:21:50 +0200

    feat: add Modal component

---
+++ frontend/src/components/Modal.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+export class Modal extends Component {
+  render() {
+    return (
+      this.props.modal && (
+        <div id="myModal" className="modal" onClick={this.props.closeModal}>
+          <div class="modal-content">
+            <h4 className="cart-title">
+              <span className="my-bag">My Bag, </span> 3items
+            </h4>
+            <div className="cart-items"></div>
+            <div className="price">
+              <p className="total-text">Total</p>
+              <p className="amount">$200</p>
+            </div>
+            <div className="button-container">
+              <button className="btn-cart btn-default btn-left">VIEW BAG</button>
+              <button className="btn-cart btn-green btn-right">CHECKOUT</button>
+            </div>
+          </div>
+        </div>
+      )
+    );
+  }
+}
+
+export default Modal;
