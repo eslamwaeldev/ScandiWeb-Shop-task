fed118a21297cc90ec058660a554f9bf5011f0f2
Eslam10497 <eslam.wael104@gmail.com>
2022-10-15 22:52:24 +0200

    feat: set up the appollo client

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from "react-dom";
 import App from "../src/components/App";
 import { store } from "../src/redux/store";
 import { Provider } from "react-redux";
+// import ApolloClient from "apollo-boost";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/",
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </ApolloProvider>,
   document.getElementById("root")
 );
