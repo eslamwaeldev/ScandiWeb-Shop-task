d59e6f4e12db0dd3c587c3bad81450aa53204b06
Eslam10497 <eslam.wael104@gmail.com>
2022-10-15 22:53:01 +0200

    feat: add all components to fetch all the items from the api

---
+++ frontend/src/components/AllItems.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import { Query } from "@apollo/react-components";
+import { FIND_ALL_ITEMS, all } from "./queries";
+import Item from "./Item";
+
+export default class AllItems extends Component {
+  render() {
+    const currency = this.props.USD ? 0 : this.props.EUR ? 1 : this.props.JPY ? 3 : 0;
+
+    return (
+      <>
+        <Query query={FIND_ALL_ITEMS} variables={{ title: all }}>
+          {({ loading, error, data }) => {
+            if (loading) return "Loading...";
+            console.log(data.category.products);
+            if (error) return <h1>Error!..</h1>;
+            return (
+              <>
+                <div className="grid-container">
+                  {data.category.products.map((product, index) => {
+                    return (
+                      <Item
+                        key={index}
+                        id={product.id}
+                        title={product.name}
+                        price={product.prices[currency].amount}
+                        currency={product.prices[currency].currency.symbol}
+                        img={product.gallery[0]}
+                      />
+                    );
+                  })}
+                </div>
+              </>
+            );
+          }}
+        </Query>
+      </>
+    );
+  }
+}
