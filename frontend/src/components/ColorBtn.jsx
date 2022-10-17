9114c2d6318ab70e23debd81c50b6917ac40c5eb
Eslam10497 <eslam.wael104@gmail.com>
2022-10-18 00:43:38 +0200

    feat: add component to choose color

---
+++ frontend/src/components/ColorBtn.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+export default class ColorBtn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: this.props.color,
+    };
+  }
+
+  render() {
+    return (
+      <>
+        <div
+          className={`${
+            this.state.color === this.props.chosenColor && "color-btn-container-chosen"
+          } color-btn-container `}
+        >
+          <button
+            className="color-btn"
+            style={{ backgroundColor: this.state.color }}
+            onClick={() => {
+              this.props.setChosen(this.state.color);
+            }}
+          ></button>
+        </div>
+      </>
+    );
+  }
+}
