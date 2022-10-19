4d20054db580fc7c51c0716db9a2071ad8d06aa9
Eslam10497 <eslam.wael104@gmail.com>
2022-10-20 00:29:38 +0200

    feat: add sizeBtn component

---
+++ frontend/src/components/SizeBtn.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+export default class SizeBtn extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      size: this.props.size,
+    };
+  }
+  render() {
+    return (
+      <>
+        <button
+          className={`${this.props.chosenSize === this.state.size && "size-btn-chosen"} size-btn`}
+          onClick={() => {
+            this.props.setChosenSize(this.state.size);
+          }}
+        >
+          {this.props.size}
+        </button>
+      </>
+    );
+  }
+}
